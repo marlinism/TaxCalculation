16dc16ee9e40273bf60c4a733dae8fac32da4d0c
marlinism <76256179+marlinism@users.noreply.github.com>
2021-12-20 22:13:30 -0800

    Add JavaSwing

--- ClientTaxCalculation.java
+++ ClientTaxCalculation.java
@@ -1,32 +1,61 @@
-import java.util.Scanner;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ClientTaxCalculation {
-
+	@SuppressWarnings("serial")
 	public static void main(String[] args) {
-		boolean mode = false;
-		
-		TaxCalculation first = new TaxCalculation();
-		
-		//header of the project
-		first.getName();
-		first.getNote();
-		
-		do {
-			//ask for the income 
-			System.out.println("--------------------------------");
-			System.out.println("How much is your income?");
-			System.out.print("$ ");
-			Scanner input = new Scanner(System.in);
-			double income = input.nextDouble();
-			first.setIncome(income);
-			
-			//calculate tax 
-			first.getTaxCalculation(income);
-			first.getEffectiveRate(income);
-		
-			//ask if they wanna calculate more
-			mode = first.calculateAgain(input);
-		} while(mode);
 
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel question = new JLabel("How much did you earn in 2021?");
+		JTextField income = new JTextField();
+		JLabel taxes = new JLabel("");
+		JButton submit = new JButton("Submit");
+
+		frame.setSize(400, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+
+		panel.setLayout(null);
+		question.setBounds(10,20, 300, 25);
+		income.setBounds(10, 55, 80, 25);
+		taxes.setBounds(10, 90, 250, 25);
+		submit.setBounds(90, 55,80, 25);
+
+		TaxCalculation first = new TaxCalculation();
+
+		submit.addActionListener( new AbstractAction("add") {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String incomeResult = income.getText();
+					double finalIncome;
+					try {
+						finalIncome = Double.parseDouble(incomeResult);
+						if(finalIncome < 0) {
+							taxes.setText("Error: Please input a positive number");
+						} else {
+							taxes.setText("You owe $" + first.getTaxCalculation(finalIncome) + " (rate: " + first.getEffectiveRate(finalIncome) + "%)");
+						}
+					} catch(Exception e1) {
+						taxes.setText("Error: Please input a number");
+					}
+				}}
+		);
+
+		panel.add(question);
+		panel.add(income);
+		panel.add(taxes);
+		panel.add(submit);
+
+		frame.setTitle("Tax Calculator");
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 }
--- TaxCalculation.java
+++ TaxCalculation.java
@@ -1,26 +1,16 @@
 
-public class TaxCalculation extends App{
-	
+public class TaxCalculation {
+
 	private double income;
-	
-   //post: get name of the program
-	public void getName() {
-		System.out.println("\tTAX SERVICE");
-	}
-	
-   //post: get note
-	public void getNote() {
-		System.out.println("Caution: This calculation is only correct when the tax payer is single");
-	}
-	
+
 	public TaxCalculation() {
 		this(0);
 	}
-	
+
 	public TaxCalculation(double incomeInput) {
 		setIncome(incomeInput);
 	}
-   
+
 	//post: set income and also check
 	public void setIncome(double incomeInput) {
       if(incomeInput < 0) {
@@ -30,12 +20,12 @@ public class TaxCalculation extends App{
 		   this.income = incomeInput;
       }
 	}
-	
+
    //post: get the income
 	public double getIncome() {
 		return this.income;
 	}
-	
+
    //pre: client's income
    //post: the amount of tax have to pay
 	private static double calculation(double income) {
@@ -49,14 +39,14 @@ public class TaxCalculation extends App{
 				income -= limit[i];
 				i++;
 			}
-			else { 
+			else {
 				result += income*rate[i];
 				income -= limit[i];
 			}
 		}
 		return result;
 	}
-	
+
    //pre: client's income
    //post: the amount of effective rate have to pay
 	private static double effectiveRate(double income) {
@@ -64,14 +54,13 @@ public class TaxCalculation extends App{
 		rate = calculation(income)/income *100;
 		return rate;
 	}
-	
-   
-	public static void getTaxCalculation(double income) {
-		System.out.println("\nThe amount of tax you need to pay is: $ " + calculation(income));
+
+
+	public static double getTaxCalculation(double income) {
+		return calculation(income);
 	}
-	
-	public static void getEffectiveRate(double income) {
-		System.out.printf("The effective rate is: %.2f",effectiveRate(income));
-		System.out.println("%\n--------------------------------");
+
+	public static double getEffectiveRate(double income) {
+		return effectiveRate(income);
 	}
 }
